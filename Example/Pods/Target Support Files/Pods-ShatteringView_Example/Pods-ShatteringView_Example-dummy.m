c4bb5539ea36e10859adc463b9855ecb7ea5fa68
Stas Batururimi <blueocean87@me.com>
2015-08-06 19:10:41 +0300

    project ample cleaned

--- Example/Pods/Target Support Files/Pods-ShatteringView_Example/Pods-ShatteringView_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ShatteringView_Example : NSObject
-@end
-@implementation PodsDummy_Pods_ShatteringView_Example
-@end
--- Example/Pods/Target Support Files/ShatteringView/ShatteringView-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "ShatteringView.h"
-
-FOUNDATION_EXPORT double ShatteringViewVersionNumber;
-FOUNDATION_EXPORT const unsigned char ShatteringViewVersionString[];
-
